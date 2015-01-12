838a36d0fba8d103422ce41e6256d8067b685891
crimsonfantasy <crimsonfantasy@Crimsonfantasy.www.crimsonfantasy.org>
2015-01-12 14:58:47 +0800

    first commit

---
+++ java/JavaSurvey/src/org/fenrir/survey/SymbolLabel.java
@@ -0,0 +1,26 @@
+import org.apache.logging.log4j.core.Logger;
+
+class SymbolLabel{
+
+	static Logger cc = Logger.getLogger(SymbolLabel.class);
+	public static void  main(String [] arg){
+
+     System.out.println("lalalala");
+    
+     
+  
+     
+}   
+
+}
+
+
+
+
+
+
+
+
+
+
+
