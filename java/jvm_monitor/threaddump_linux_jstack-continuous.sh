7fa5cd503b81fb56647180299ecfd59006ae97cf
fenrircheng <fenrircheng@jumbogames.com.tw>
2018-04-16 16:19:13 +0800

    save

---
+++ java/jvm_monitor/threaddump_linux_jstack-continuous.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Takes the JBoss PID as an argument. 
+#
+# Make sure you set JAVA_HOME
+#
+# Create thread dumps a specified number of times (i.e. LOOP) and INTERVAL. 
+#
+# Thread dumps will be collected in the file "jstack_threaddump.out", in the same directory from where this script is been executed.
+#
+# Usage: sh ./threaddump_linux_jstack-continuous.sh <JBOSS_PID>
+#
+
+# Number of times to collect data.
+LOOP=6
+# Interval in seconds between data points.
+INTERVAL=1
+# Setting the Java Home, by giving the path where your JDK is kept
+JAVA_HOME=/usr/lib/jvm/java-8-oracle
+
+i=0
+while [ $i -lt $LOOP ]
+do
+   i=$(expr $i + 1)
+   $JAVA_HOME/bin/jstack -l $1 >> jstack_threaddump.out
+   echo "thread dump #" $i
+   if [ $i -lt $LOOP ]; then
+    echo "sleeping..."
+    #sleep $INTERVAL
+  fi
+done
